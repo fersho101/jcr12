801e08a2f400adcbb81d6fda16bfef70f74951e3
fernando corral <146652466+fersho101@users.noreply.github.com>
2024-11-03 00:44:48 -0700

    first commit

---
+++ src/com/fersho/c01/Example.java
@@ -0,0 +1,15 @@
+package com.fersho.c01;
+
+/**
+ * @author fersho
+ */
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("this is a simple Java program");
+    }
+}
+
+// To compile:
+// javac -d C:\Users\fdoco\Documents\java-complete-reference-12e\jcr12\bin\ com\fersho\c01\Example.java
+// To run
+// java -cp C:\Users\fdoco\Documents\java-complete-reference-12e\jcr12\bin com.fersho.c01.Example
